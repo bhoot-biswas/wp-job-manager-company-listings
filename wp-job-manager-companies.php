5ac93c57675273286fc91e27968755141ba2709c
Mithun Biswas <bhoot.biswas@gmail.com>
2020-10-11 21:06:42 +0600

    Add initial files

---
+++ wp-job-manager-companies.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Job Manager Companies
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-job-manager-companies
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Job_Manager_Companies
+ */
+
+// Your code starts here.
