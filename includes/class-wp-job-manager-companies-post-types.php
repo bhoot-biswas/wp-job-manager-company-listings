89d49944585da5b54a84b18d65acb420533a9d0c
Mithun Biswas <bhoot.biswas@gmail.com>
2020-10-11 21:33:53 +0600

    Add WP_Job_Manager_Companies_Post_Types class

---
+++ includes/class-wp-job-manager-companies-post-types.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * File containing the class WP_Job_Manager_Companies_Post_Types.
+ *
+ * @package WP_Job_Manager_Companies
+ */
+
+namespace BengalStudio;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * HHandles displays and hooks for the Company Listing custom post type.
+ * @var [type]
+ */
+class WP_Job_Manager_Companies_Post_Types {
+
+	/**
+	 * The single instance of the class.
+	 * @var [type]
+	 */
+	private static $instance = null;
+
+	/**
+	 * Allows for accessing single instance of class. Class should only be constructed once per call.
+	 * @return [type] [description]
+	 */
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		add_action( 'init', [ $this, 'register_post_types' ], 0 );
+	}
+
+}
