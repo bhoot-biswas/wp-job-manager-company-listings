cb9951cdf406ded246a5be4c1180243de837fe54
Mithun Biswas <bhoot.biswas@gmail.com>
2020-10-12 23:46:34 +0600

    Add get_the_company_description()

--- wp-job-manager-companies-template.php
+++ wp-job-manager-companies-template.php
@@ -24,3 +24,28 @@ function the_company_description( $post = null ) {
 		WP_Job_Manager_Post_Types::output_kses_post( $company_description );
 	}
 }
+
+/**
+ * Gets the company description for the listing.
+ *
+ * @since 0.1.0
+ * @param int|WP_Post $post (default: null).
+ * @return string|bool|null
+ */
+function get_the_company_description( $post = null ) {
+	$post = get_post( $post );
+	if ( ! $post || 'company_listing' !== $post->post_type ) {
+		return null;
+	}
+
+	$description = apply_filters( 'the_company_description', wp_kses_post( $post->post_content ) );
+
+	/**
+	 * Filter for the company description.
+	 *
+	 * @since 0.1.0
+	 * @param string      $company_description Job description to be filtered.
+	 * @param int|WP_Post $post
+	 */
+	return apply_filters( 'wpjm_the_company_description', $description, $post );
+}
